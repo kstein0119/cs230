72693655a652819fee5061ebab62c5615ec56c16
kstein0119 <kstein0119@gmail.com>
2021-02-02 18:24:43 -0500

    Post-lab video changes

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<head>
+    <title>This is a title.</title>
+</head>
+
 <body>
     <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
         <h1>About Us</h1>
         <p>alskdjf;lasdkfj;asdklfj;</p>
+    </div>
+    <div class="row">
+        <button type="button">What Does This Do?</button>
     </div>
     <div class="row">
         <div class="column">
@@ -13,6 +20,7 @@
                     <h2>CEO</h2>
                     <p>Kylo Ren</p>
                     <p>Email: benswolo@cs230.com</p>
+                    <p>This is the swolest man I have ever seen. My god, he is so swole.</p>
                 </div>
             </div>
         </div>
@@ -20,9 +28,10 @@
             <div class="card">
                 <img src="images/tohru.jpg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p>Kylo Ren</p>
-                    <p>Email: benswolo@cs230.com</p>
+                    <h2>CFO</h2>
+                    <p>Tohru</p>
+                    <p>Email: dragontales@cs230.com</p>
+                    <p>Give the dragon lady your money. Just do it.</p>
                 </div>
             </div>
         </div>
@@ -30,9 +39,10 @@
             <div class="card">
                 <img src="images/asmongold.jpg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p>Kylo Ren</p>
-                    <p>Email: benswolo@cs230.com</p>
+                    <h2>Court Jester</h2>
+                    <p>Asmongold</p>
+                    <p>Email: goldilocks@cs230.com</p>
+                    <p>How'd this guy even get a job? Weirdo.</p>
                 </div>
             </div>
         </div>
@@ -40,9 +50,10 @@
             <div class="card">
                 <img src="images/doughboy.jpg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p>Kylo Ren</p>
-                    <p>Email: benswolo@cs230.com</p>
+                    <h2>Mascot</h2>
+                    <p>Pillsbury Doughboy</p>
+                    <p>Email: yeasty@cs230.com</p>
+                    <p>Get that bread, king. A true patrio
                 </div>
             </div>
         </div>
