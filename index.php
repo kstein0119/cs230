54e573c3bcdb33a6a0838253b8e1afb6b928922b
kstein0119 <kstein0119@gmail.com>
2021-02-02 18:13:10 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>alskdjf;lasdkfj;asdklfj;</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/swolo.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kylo Ren</p>
+                    <p>Email: benswolo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/tohru.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kylo Ren</p>
+                    <p>Email: benswolo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/asmongold.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kylo Ren</p>
+                    <p>Email: benswolo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/doughboy.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Kylo Ren</p>
+                    <p>Email: benswolo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
